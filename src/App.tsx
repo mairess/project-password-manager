45b437247022c4a2dbf2967af5a105fe225e6bc9
Maires Souza <trabalho.maires@gmail.com>
2023-08-18 07:08:49 -0300

    component: form implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Form from './components/Form';
 import Title from './components/Title';
 
 function App() {
   return (
     <div>
       <Title />
-      Hello World
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,31 @@
+function Form() {
+  return (
+    <fieldset>
+      <legend />
+      <label htmlFor="service-name-input">
+        Nome do serviço
+        <input type="text" name="" id="service-name-input" />
+      </label>
+
+      <label htmlFor="login-input">
+        Login
+        <input type="text" name="" id="login-input" />
+      </label>
+
+      <label htmlFor="passwprd-input">
+        Senha
+        <input type="password" name="" id="passwprd-input" />
+      </label>
+
+      <label htmlFor="url-input">
+        URL
+        <input type="text" name="" id="url-input" />
+      </label>
+
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </fieldset>
+  );
+}
+
+export default Form;
