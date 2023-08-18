ceeca75114084c20902d70ffdba29343cd30042c
Maires Souza <trabalho.maires@gmail.com>
2023-08-18 09:29:45 -0300

    feat: disable button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,75 @@
+import { useState } from 'react';
+
 type FormProps = {
   onCancel: () => void;
 };
 
 function Form({ onCancel }: FormProps) {
+  const [serviceNameInput, setServiceNameInput] = useState('');
+  const [loginInput, setLoginInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [urlInput, setUrlInput] = useState('');
+  const regexSpecialChar = /[^a-zA-Z ]/g;
+  const haveSpecialChar = regexSpecialChar.test(passwordInput);
+
+  const isFormValid = serviceNameInput.length > 0
+    && loginInput.length > 0
+    && passwordInput.length >= 8
+    && urlInput.length > 0
+    && !haveSpecialChar;
+
   return (
     <fieldset>
       <legend />
       <label htmlFor="service-name-input">
         Nome do serviço
-        <input type="text" name="" id="service-name-input" />
+        <input
+          type="text"
+          name=""
+          id="service-name-input"
+          value={ serviceNameInput }
+          onChange={ ({ target: { value } }) => setServiceNameInput(value) }
+        />
       </label>
 
       <label htmlFor="login-input">
         Login
-        <input type="text" name="" id="login-input" />
+        <input
+          type="text"
+          name=""
+          value={ loginInput }
+          id="login-input"
+          onChange={ ({ target: { value } }) => setLoginInput(value) }
+        />
       </label>
 
       <label htmlFor="passwprd-input">
         Senha
-        <input type="password" name="" id="passwprd-input" />
+        <input
+          type="password"
+          name=""
+          value={ passwordInput }
+          id="passwprd-input"
+          maxLength={ 16 }
+          onChange={ ({ target: { value } }) => setPasswordInput(value) }
+        />
       </label>
 
       <label htmlFor="url-input">
         URL
-        <input type="text" name="" id="url-input" />
+        <input
+          type="text"
+          name=""
+          value={ urlInput }
+          onChange={ ({ target: { value } }) => setUrlInput(value) }
+          id="url-input"
+        />
       </label>
-
-      <button>Cadastrar</button>
+      <button
+        disabled={ !isFormValid }
+      >
+        Cadastrar
+      </button>
       <button onClick={ onCancel }>Cancelar</button>
     </fieldset>
   );
